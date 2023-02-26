ca07fae0b92b212fd64e5fab6770658a7adcee19
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-02-26 20:29:39 -0300

    added the elements in the app using Js dom

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
 const title = document.createElement('h1');
 const textTitle = document.createTextNode('Contador em Javascript');
+const numCounter = document.createElement('h2');
+numCounter.id = 'output';
+const textNumCounter = document.createTextNode('0');
 const divWithClass = document.createElement('div');
-divWithClass.className = 'containter-contador'
+divWithClass.className = 'container-contador'
 
 const btnAdd = document.createElement('button');
 btnAdd.id = 'adicionar'
-const textBtnAdd = document.createTextNode('+')
+const textBtnAdd = document.createTextNode('Incrementar')
 const btnSubtract = document.createElement('button');
 btnSubtract.id = 'subtrair'
-const textBtnSub = document.createTextNode('-')
-const spanOutput = document.createElement('span');
-spanOutput.id = 'output'
-const textSpan = document.createTextNode('0')
+const textBtnSub = document.createTextNode('Decrementar')
 const sectionMain = document.querySelector("main");
 const subtrair = document.querySelector("#subtrair");
 const adicionar = document.querySelector("#add");
 const output = document.querySelector("#output");
+const btnZerar = document.createElement('button');
+const textBtnZerar = document.createTextNode('Zerar');
 
 //Inserindo os textos nos elementos criados
 title.appendChild(textTitle);
 btnAdd.appendChild(textBtnAdd);
 btnSubtract.appendChild(textBtnSub);
-spanOutput.appendChild(textSpan)
+numCounter.appendChild(textNumCounter);
+btnZerar.appendChild(textBtnZerar);
 
 //Inserindo os elementos dentro da div principal
 divWithClass.appendChild(title)
+divWithClass.appendChild(numCounter)
 divWithClass.appendChild(btnSubtract)
-divWithClass.appendChild(spanOutput)
+divWithClass.appendChild(btnZerar)
 divWithClass.appendChild(btnAdd)
 
 sectionMain.appendChild(divWithClass)
 
 function addAndSubtract () {
+    let result = 0;
     btnAdd.addEventListener("click", function() {
-        let result = Number(spanOutput.innerText) + 1;
+        result = Number(numCounter.innerText) + 1;
 
-        if (result > 10) {
-            result = 10
-        }
-        spanOutput.innerText = result
+        if (result > 100) {
+            result = 100
+        }        
+        numCounter.innerText = result
     })
     btnSubtract.addEventListener("click", function() {
-        let result = Number(spanOutput.innerText) - 1;
+        result = Number(numCounter.innerText) - 1;
 
         if (result < 0) {
             result = 0
         }
-        spanOutput.innerText = result;
+        numCounter.innerText = result;
     })
+    btnZerar.addEventListener("click", function() {
+        result = 0;
+
+        numCounter.innerText = result;
+    })
+
+    if (Number(numCounter.innerText) % 2 === 0) {
+        numCounter.style.color = "green";
+    }
+    if (Number(numCounter.innerText) % 2 !== 0) {
+        numCounter.style.color = "red"
+    }
 }
 
-addAndSubtract();
+addAndSubtract()
